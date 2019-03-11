4333e0c9c103b87600e39a4eab3bf411f3ea3e22
Alexandr <krak1111@yandex.ru>
2019-03-11 22:45:22 +0300

    Add a link parsing output view [{'Discipline': [{'Journal title' : 'title', 'Journal url' : 'url'}, ... ]}, ....]

--- journals_name.py
+++ journals_name.py
@@ -12,11 +12,12 @@ DISCIPLINES = [
     'mathematics',
 ]
 
-BASE_URL = 'https://www.sciencedirect.com/'
+BASE_URL = 'https://www.sciencedirect.com'
 HEADERS = {'user-agent': 'Mozilla/5.0'}
 
 def journals_titles(disciplines, headers, base_url):
     parse_pesult = []
+
     for discipline in disciplines:
         parse_pesult.append({f'{discipline}' : parse(discipline, headers, base_url)})
 
@@ -27,22 +28,22 @@ def parse(discipline, headers, base_url):
     k = 1
     last_page_num = 5
     i = 1
-    titles = []
+    
+    journals_info = []
 
     #Pagination cycle k < 5 for garanty uninfinity cycle
-    while k < 5 and i <= last_page_num:
+    while i <= last_page_num:
 
-        url = f'{base_url}browse/journals-and-books?page={i}&contentType=JL&subject={discipline}'
+        url = f'{base_url}/browse/journals-and-books?page={i}&contentType=JL&subject={discipline}'
         request = session.get(url, headers = headers)
         html = request.html
-        selector = 'a.js-publication-title > span.anchor-text' # css class anchor-text not unique for journal title
+        selector = 'a.js-publication-title' # css class anchor-text not unique for journal title
 
-        title_elements = html.find(selector) #find all elements
+        journal_elements = html.find(selector) #find all elements
 
         #parse titles
-        for title_element in title_elements:
-            title_url_form = title_element.text.lower().replace(' ', '-') #For examle: Physics and Astronomy bring to form "physics-and-astronomy"
-            titles.append(title_url_form)
+        for journal_element in journal_elements:            
+            journals_info.append({'Journal title' :journal_element.text, 'Journal url' : f'{base_url}{journal_element.links.pop()}'})
 
         #pagination check only on first iteration
         if i == 1 :
@@ -52,9 +53,12 @@ def parse(discipline, headers, base_url):
             except IndexError: # if only one page we have not a pagination element
                 break
         i+=1
-        k+=1
+    
 
-    return titles
+    return journals_info
+
+
+
 
 if __name__ == '__main__':
     journals_titles(DISCIPLINES, HEADERS, BASE_URL)
