8d71c031fe43f56e7734313e0b8e7372558e00bb
Alexandr <krak1111@yandex.ru>
2019-03-12 21:58:57 +0300

    parsing issues urls and names need to remove time.sleep(30) (line 72)

--- issues_parsing.py
+++ issues_parsing.py
@@ -1,7 +1,12 @@
 import re
+import time
+import json
+
+
 from requests_html import *
 
 from settings import *
+
 
 
 JOURNAL_URL = f'{BASE_URL}/journal/advances-in-climate-change-research/issues'
@@ -9,7 +14,10 @@ JOURNAL_URL = f'{BASE_URL}/journal/advances-in-climate-change-research/issues'
 
 def issn_search(page):
     selector = 'p.js-issn'
-    issn = page.find(selector, first = True).text.replace('-','')
+    issn_pattern = re.compile(r'([0-9]+)-([0-9]+)')
+
+    issn_text = page.find(selector, first = True).text
+    issn = issn_pattern.search(issn_text).group().replace('-','')
 
     return issn
 
@@ -23,14 +31,52 @@ def years(page):
     for volume in volumes:
         year = year_pattern.search(volume.text).group() #Find the year from title accordint to the pattern
         years.append(year)
-        print(year)
-
+        
     return years
 
+def last_year_parsing(page, year):
+    selector = 'div.issue-item > a.anchor' 
+
+    volume = page.find(selector)
+
+    issue_list = []
+
+    for issue in volume:
+        issue_dict = {'name': f'{issue.text}', 'url' : f'{issue.links.pop()}'}
+        issue_list.append(issue_dict)
+        
+    return issue_list
+
+
+def others_years_parsing(issn, years):
+    session = HTMLSession()
+    volume_data = []
+    for year in years:
+        year_url = f'{BASE_URL}/journal/{issn}/year/{year}/issues'
+        print(year_url)
+        request = session.get(year_url, headers = HEADERS) #reply will be a JSON object
+        
+        volume_json = request.text #get json format text
+       
+        json_object = json.loads(volume_json) #read json
+        
+        issue_data = []
+
+        for data in json_object["data"]:
+            issue_name = data["volIssueSupplementText"]
+            issue_url = data["uriLookup"]
+            issue_data.append({'name' : issue_name, 'url' : issue_url})
+            
+        
+        volume_data.append({f'{year}' : issue_data})
+        time.sleep(30) # I don't want to ban
+
+    return volume_data        
 
 
 def main(url):
     session = HTMLSession()
+
     try:
         request = session.get(url, headers = HEADERS)
     
@@ -41,14 +87,13 @@ def main(url):
     issn = issn_search(page)
     print(issn)
     
-    publication_years = years(page)[1:] # Last year already render on the page, parse below
+    publication_years = years(page) 
 
-    
+    last_year = publication_years.pop(0) # Last year already render on the page, parse below
 
+    issues = [{f'{last_year}': last_year_parsing(page, last_year)}]
 
-    
-
-
+    issues.append(others_years_parsing(issn, publication_years))
     
 
 if __name__ == '__main__':
