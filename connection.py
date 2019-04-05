6ed0bef949eaf1ae52ac848eddd132ac494c3e74
Alexandr <krak1111@yandex.ru>
2019-04-05 23:23:35 +0300

    Fix error with text in get_request func

--- connection.py
+++ connection.py
@@ -56,6 +56,7 @@ def get_request(url):
     while True:
         try:
             request = session.get(url, headers=support.give_header())
+            text = request.text
             if 'There was a problem providing the content you requested' in text:
                 print('Banned')
                 
